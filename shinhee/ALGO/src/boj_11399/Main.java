ad1739843cd6b2f0d7cab3d700d423981653a056
shinheekim <shinhee1241@gmail.com>
2024-12-16 23:57:10 +0900

    [BOJ] 11399 ATM / 실버 4 / 15분

---
+++ shinhee/ALGO/src/boj_11399/Main.java
@@ -0,0 +1,33 @@
+package ALGO.src.boj_11399;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        long sum = 0;
+        long total = 0;
+
+        for (int i = 0; i < N; i++) {
+            sum += arr[i];
+            total += sum;
+        }
+
+        System.out.println(total);
+    }
+}
