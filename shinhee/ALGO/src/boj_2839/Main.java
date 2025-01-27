598a12ba7ce1dcc69ee4a568f3cb1e6d8a4895fc
shinheekim <dev.shinheekim@gmail.com>
2025-01-27 22:51:47 +0900

    [BOJ] 9625 BABBA / 실버 5 /15분

--- shinhee/ALGO/src/boj_2839/Main.java
+++ shinhee/ALGO/src/boj_2839/Main.java
@@ -1,40 +1,21 @@
 package ALGO.src.boj_2839;
 
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-
-    static Integer[][] arr = new Integer[41][2];
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        arr[0][0] = 1;
-        arr[0][1] = 0;
-        arr[1][0] = 0;
-        arr[1][1] = 1;
-
-        int T = Integer.parseInt(br.readLine());
-
-        StringBuilder sb = new StringBuilder();
-
-        while(T --> 0){
-            int N = Integer.parseInt(br.readLine());
-            dp(N);
-            sb.append(arr[N][0] + " " + arr[N][1]).append('\n');
+        int k = Integer.parseInt(br.readLine());
+        int[][] dp = new int[2][46];
+        dp[0][2] = 1;
+        dp[1][1] = 1;
+        dp[1][2] = 1;
+        for (int i = 3; i <= k; i++) {
+            dp[0][i] = dp[0][i-2] + dp[0][i-1];
+            dp[1][i] = dp[1][i-2] + dp[1][i-1];
         }
-        System.out.println(sb);
+        System.out.print(dp[0][k] + " " + dp[1][k]);
     }
-
-    static Integer[] dp(int N) {
-        if(arr[N][0] == null || arr[N][1] == null) {
-            arr[N][0] = dp(N - 1)[0] + dp(N - 2)[0];
-            arr[N][1] = dp(N - 1)[1] + dp(N - 2)[1];
-        }
-        return arr[N];
-
-    }
-
 }
