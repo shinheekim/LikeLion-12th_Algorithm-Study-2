74f323c928bf073e0f8bf87f864a6fc89d706a66
cini <shinhee1241@gmail.com>
2025-02-03 23:35:03 +0900

    [BOJ] 15649 N과 M (1)/ 실버 3 / 20분

---
+++ shinhee/ALGO/src/boj_25649/Main.java
@@ -0,0 +1,44 @@
+package ALGO.src.boj_25649;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N, M;
+    static int[] arr;
+    static boolean[] checked;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[M];
+        checked = new boolean[N + 1];
+
+        backTracking(0);
+    }
+
+    static void backTracking(int depth) {
+        if (depth == M) {
+            for (int i = 0; i < M; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+            return;
+        }
+        for (int i = 1; i <= N; i++) {
+            if (!checked[i]) {
+                checked[i] = true;
+                arr[depth] = i;
+                backTracking(depth + 1);
+                checked[i] = false;
+            }
+        }
+    }
+}
